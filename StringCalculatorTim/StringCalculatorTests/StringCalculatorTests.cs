35e7675a8c5e858ff5a6a5ce785ec258e6fb2d4e
Tim Allen <tallen@qualtrax.com>
2016-03-01 14:55:40 -0500

    Step 1 complete

---
+++ StringCalculatorTim/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private Calculator calculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.calculator = new Calculator();
+        }
+
+        [TestMethod]
+        public void TestAddEmptyString()
+        {
+            var sum = calculator.Add(String.Empty);
+
+            Assert.AreEqual(0, sum);
+        }
+
+        [TestMethod]
+        public void TestAddOne()
+        {
+            var sum = calculator.Add("1");
+
+            Assert.AreEqual(1, sum);
+        }
+
+        [TestMethod]
+        public void TestAddOneAndTwo()
+        {
+            var sum = calculator.Add("1,2");
+
+            Assert.AreEqual(3, sum);
+        }
+    }
+}
---
+++ StringCalculatorTim/StringCalculatorTim/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public Int32 Add(String stringNumbers)
+        {
+            if (String.IsNullOrEmpty(stringNumbers))
+                return 0;
+
+            var splitNumbers = stringNumbers.Split(',');
+
+            return splitNumbers.Sum(o => Convert.ToInt32(o));      
+        }
+    }
+}
