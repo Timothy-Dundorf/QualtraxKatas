b65187400d78b611f7478741baafa434ca47cf0d
t1mothyjam3s <dundorft@vt.edu>
2016-03-02 16:01:36 -0500

    Completed up to step 4 on my own. Waiting for Tim Allen to review.

--- StringCalculatorTim/StringCalculatorTests/StringCalculatorTests.cs
+++ StringCalculatorTim/StringCalculatorTests/StringCalculatorTests.cs
@@ -13,6 +13,7 @@ namespace StringCalculatorTests
         public void Initialize()
         {
             this.calculator = new Calculator();
+
         }
 
         [TestMethod]
@@ -37,6 +38,41 @@ namespace StringCalculatorTests
             var sum = calculator.Add("1,2");
 
             Assert.AreEqual(3, sum);
+        }
+
+        [TestMethod]
+        public void TestAddOneTwoThree()
+        {
+            var sum = calculator.Add("1,2,3");
+
+            Assert.AreEqual(6, sum);
+        }
+
+        /// <summary>
+        /// I solved this problem by creating a char array with space and comma to input into the parameter that converts the string into an array.
+        /// </summary>
+        [TestMethod]
+        public void TestAddNewLineSeperated()
+        {
+            var sum = calculator.Add("1 2");
+
+            Assert.AreEqual(3, sum);
+        }
+
+        [TestMethod]
+        public void TestSupportDifferentDelimeter()
+        {
+            var sum = calculator.Add("//; 1;2");
+
+            Assert.AreEqual(3, sum);
+        }
+
+        [TestMethod]
+        public void TestSupportDifferentAndVariedDelimeters()
+        {
+            var sum = calculator.Add("//; 1;2,3 1");
+
+            Assert.AreEqual(7, sum);
         }
     }
 }
--- StringCalculatorTim/StringCalculatorTim/Calculator.cs
+++ StringCalculatorTim/StringCalculatorTim/Calculator.cs
@@ -13,7 +13,15 @@ namespace StringCalculator
             if (String.IsNullOrEmpty(stringNumbers))
                 return 0;
 
-            var splitNumbers = stringNumbers.Split(',');
+            var separators = new char[] { ' ', ',' };
+
+            if (stringNumbers[0] == '/')
+            {
+                var newSeperator = stringNumbers[2];
+                separators = new char[] { ' ', ',', newSeperator};
+                stringNumbers = stringNumbers.Remove(0, 4);
+            }
+            var splitNumbers = stringNumbers.Split(separators);
 
             return splitNumbers.Sum(o => Convert.ToInt32(o));      
         }
